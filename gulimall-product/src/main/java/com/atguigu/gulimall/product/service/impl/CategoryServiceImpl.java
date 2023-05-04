c7dc88a0bf732030cb7a203904a549fb505bccb1
qiaoJiancheng <qiaojc3@163.com>
2023-05-04 20:00:43 +0800

    fit: 功能更新

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,18 +1,20 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.atguigu.gulimall.product.dao.CategoryBrandRelationDao;
-import com.atguigu.gulimall.product.service.CategoryBrandRelationService;
 import com.atguigu.gulimall.product.vo.Catelog2Vo;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -24,6 +26,7 @@ import com.atguigu.gulimall.product.dao.CategoryDao;
 import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.CategoryService;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 
 @Service("categoryService")
@@ -34,6 +37,8 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     @Autowired
     private CategoryBrandRelationDao categoryBrandRelationDao;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -101,30 +106,67 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     @Override
     public Map<String, List<Catelog2Vo>> getCatelogJson() {
-        // 查询一级分类
-        List<CategoryEntity> level1Categorys = this.getLevel1Categorys();
-        Map<String, List<Catelog2Vo>> catelogJson = level1Categorys.stream().collect(Collectors.toMap(k -> k.getCatId().toString(), v -> {
-            List<Catelog2Vo> catelog2Vos = null;
-            // 获取一级菜单分类下的二级分类菜单
-            List<CategoryEntity> categoryEntities2 = this.list(new QueryWrapper<CategoryEntity>().eq("parent_cid", v.getCatId()));
-            if (categoryEntities2 != null) {
-                catelog2Vos = categoryEntities2.stream().map(categoryEntity2 -> {
-                    Catelog2Vo catelog2Vo = new Catelog2Vo(categoryEntity2.getParentCid().toString(), null, categoryEntity2.getCatId().toString(), categoryEntity2.getName());
-                    // 获取当前二级菜单下的三级菜单并封装成vo
-                    List<CategoryEntity> categoryEntities3 = this.list(new QueryWrapper<CategoryEntity>().eq("parent_cid", categoryEntity2.getCatId()));
-                    if (categoryEntities3 != null) {
-                        List<Catelog2Vo.Catelog3Vo> collect = categoryEntities3.stream().map(categoryEntity3 -> {
-                            Catelog2Vo.Catelog3Vo catelog3Vo = new Catelog2Vo.Catelog3Vo(categoryEntity2.getCatId().toString(), categoryEntity3.getCatId().toString(), categoryEntity3.getName());
-                            return catelog3Vo;
-                        }).collect(Collectors.toList());
-                        catelog2Vo.setCatalog3List(collect);
-                    }
-                    return catelog2Vo;
-                }).collect(Collectors.toList());
+        // 从缓存中查询
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        String catelogJSON = ops.get("catelogJSON");
+        if (StringUtils.isEmpty(catelogJSON)) {
+            // 缓存中数据错误，需要重新查询数据库获取数据
+            Map<String, List<Catelog2Vo>> catelogJsonFromDb = getCatelogJsonFromDb();
+            // 将查询到的数据进行返回
+            return catelogJsonFromDb;
+        }
+        // 将从缓存中查询到的数据进行转换并返回
+        return JSON.parseObject(catelogJSON, new TypeReference<Map<String, List<Catelog2Vo>>>() {
+        });
+    }
+
+    public Map<String, List<Catelog2Vo>> getCatelogJsonFromDb() {
+
+        synchronized (this) {
+
+            // 从缓存中查询
+            ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+            String catelogJSON = ops.get("catelogJSON");
+            if (!StringUtils.isEmpty(catelogJSON)) {
+                // 将从缓存中查询到的数据进行转换并返回
+                return JSON.parseObject(catelogJSON, new TypeReference<Map<String, List<Catelog2Vo>>>() {
+                });
             }
-            return catelog2Vos;
-        }));
-        return catelogJson;
+
+            // 将多次DB查询变为一次
+            List<CategoryEntity> categoryEntities = this.baseMapper.selectList(null);
+
+            // 查询一级分类
+            List<CategoryEntity> level1Categorys = getParent_cid(categoryEntities, 0L);
+            Map<String, List<Catelog2Vo>> catelogJson = level1Categorys.stream().collect(Collectors.toMap(k -> k.getCatId().toString(), v -> {
+                List<Catelog2Vo> catelog2Vos = null;
+                // 获取一级菜单分类下的二级分类菜单
+                List<CategoryEntity> categoryEntities2 = getParent_cid(categoryEntities, v.getCatId());
+                if (categoryEntities2 != null) {
+                    catelog2Vos = categoryEntities2.stream().map(l2 -> {
+                        Catelog2Vo catelog2Vo = new Catelog2Vo(l2.getParentCid().toString(), null, l2.getCatId().toString(), l2.getName());
+                        // 获取当前二级菜单下的三级菜单并封装成vo
+                        List<CategoryEntity> categoryEntities3 = getParent_cid(categoryEntities, l2.getCatId());
+                        if (categoryEntities3 != null) {
+                            List<Catelog2Vo.Catelog3Vo> collect = categoryEntities3.stream().map(categoryEntity3 -> {
+                                Catelog2Vo.Catelog3Vo catelog3Vo = new Catelog2Vo.Catelog3Vo(l2.getCatId().toString(), categoryEntity3.getCatId().toString(), categoryEntity3.getName());
+                                return catelog3Vo;
+                            }).collect(Collectors.toList());
+                            catelog2Vo.setCatalog3List(collect);
+                        }
+                        return catelog2Vo;
+                    }).collect(Collectors.toList());
+                }
+                return catelog2Vos;
+            }));
+            // 将获取的数据存入缓存中
+            ops.set("catelogJSON", JSON.toJSONString(catelogJson), 1, TimeUnit.DAYS);
+            return catelogJson;
+        }
+    }
+
+    private List<CategoryEntity> getParent_cid(List<CategoryEntity> categoryEntities, Long parentCid) {
+        return categoryEntities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == parentCid).collect(Collectors.toList());
     }
 
     public List<Long> findParentPath(Long catelogId, List<Long> paths) {
--- gulimall-product/src/test/java/com/atguigu/product/ProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/product/ProductApplicationTests.java
@@ -8,9 +8,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
+import java.util.UUID;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -20,10 +23,21 @@ public class ProductApplicationTests {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     public void testFindPath() {
         Long[] categoryPath = categoryService.findCategoryPath(225L);
         log.info("完整路径：{}", Arrays.asList(categoryPath));
+    }
+
+    @Test
+    public void testSpringRedisTemplate() {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("hello", UUID.randomUUID().toString());
+
+        log.info("之前保存的数据：{}", ops.get("hello"));
     }
 
 }
